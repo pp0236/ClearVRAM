da0f5fe029a9c00e7f08ed60a7b59fe0a4eb416c
pp021 <pp0236.lol@gmail.com>
2021-12-10 13:32:41 +0700

    update

--- ClearVRAM/ClearVRAM.cs
+++ ClearVRAM/ClearVRAM.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -8,41 +8,32 @@ using UIExpansionKit.API;
 using VRC;
 using Object = UnityEngine.Object;
 
-namespace ClearVRAM
-{
-    public static class BuildInfo
-    {
+namespace ClearVRAM {
+    public static class BuildInfo {
         public const string Name = "ClearVRAM";
-        public const string Author = "Requi";
+        public const string Author = "Requi / pp021 edit";
         public const string Company = "RequiDev";
-        public const string Version = "1.0.5";
+        public const string Version = "1.0.6";
         public const string DownloadLink = "https://github.com/RequiDev/ClearVRAM";
     }
 
-    public class ClearVRAM : MelonMod
-    {
-        public override void OnApplicationStart()
-        {
-            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Clear VRAM", () =>
-            {
-                var currentAvatars = (from player in PlayerManager.prop_PlayerManager_0.prop_ArrayOf_Player_0 where player != null select player.prop_ApiAvatar_0 into apiAvatar where apiAvatar != null select apiAvatar.assetUrl).ToList();
+    public class ClearVRAM : MelonMod {
+        public override void OnApplicationStart() {
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Clear VRAM", () => {
+                var currentAvatars = (from player in PlayerManager.field_Private_Static_PlayerManager_0.prop_ArrayOf_Player_0 where player != null select player.prop_ApiAvatar_0 into apiAvatar where apiAvatar != null select apiAvatar.assetUrl).ToList();
 
                 var dict = new Dictionary<string, AssetBundleDownload>();
                 var abdm = AssetBundleDownloadManager.prop_AssetBundleDownloadManager_0;
-                foreach (var key in abdm.field_Private_Dictionary_2_String_AssetBundleDownload_0.Keys)
-                {
+                foreach (var key in abdm.field_Private_Dictionary_2_String_AssetBundleDownload_0.Keys) {
                     dict.Add(key, abdm.field_Private_Dictionary_2_String_AssetBundleDownload_0[key]);
                 }
 
-                foreach (var key in dict.Keys)
-                {
+                foreach (var key in dict.Keys) {
                     var assetBundleDownload = abdm.field_Private_Dictionary_2_String_AssetBundleDownload_0[key];
 
-                    if (!assetBundleDownload.field_Private_String_0.Contains("wrld_") && !currentAvatars.Contains(key))
-                    {
+                    if (!assetBundleDownload.field_Private_String_0.Contains("wrld_") && !currentAvatars.Contains(key)) {
                         abdm.field_Private_Dictionary_2_String_AssetBundleDownload_0.Remove(key);
-                        if (assetBundleDownload.field_Private_GameObject_0 != null)
-                        {
+                        if (assetBundleDownload.field_Private_GameObject_0 != null) {
                             Object.DestroyImmediate(assetBundleDownload.field_Private_GameObject_0, true);
                         }
                     }
